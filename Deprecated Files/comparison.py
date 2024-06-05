f23aa5524bfb3bc22c0b36be8636934e49263dea
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2024-06-05 13:22:09 -0600

    Comenta y cambia nombre de archivos

--- Deprecated Files/comparison.py
+++ Deprecated Files/comparison.py
@@ -1,5 +1,6 @@
 import os
 
+# Cuenta el numeor de archivos con la extension del directorio
 def count_files(directory, extension):
     count = 0
     for root, dirs, files in os.walk(directory):
@@ -8,8 +9,14 @@ def count_files(directory, extension):
                 count += 1
     return count
 
+# Count the number of .c files in the 'fire14-source-code-training-dataset\c' directory
 c_files = count_files('fire14-source-code-training-dataset\c', '.c')
+
+# Count the number of .java files in the 'fire14-source-code-training-dataset\java' directory
 java_files = count_files('fire14-source-code-training-dataset\java', '.java')
 
+# Calculate the proportion of .c files to .java files
 proportion = c_files / java_files
+
+# Print the calculated proportion
 print(f"The proportion of the number of .c files to .java files in the fire14 directory is: {proportion}")
